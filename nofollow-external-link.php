1b6658bc0e11632c5081aa2b0541fc0dfeefdaf5
David Herron <david@davidherron.com>
2015-09-05 12:00:21 -0700

    Bump version number for testing wp-pusher

--- nofollow-external-link.php
+++ nofollow-external-link.php
@@ -3,7 +3,7 @@
 Plugin Name: DSHNofollow 
 Plugin URI: http://davidherron.com
 Description: Control which external links have <code>rel=&quot;nofollow&quot;</code> and <code>target=&quot;_blank&quot;</code> aded to them.  It can be configured so all external links get these attributes, and a white-list and black-list give finer grained control.  The <strong>white list domains</strong>, if specified, will not to get the <code>rel=&quot;nofollow&quot;</code> attribute.  The <strong>black list domains</strong>, if specified, is a precise list of the domains which get the <code>rel=&quot;nofollow&quot;</code> attribute.  If no black list is specified, then all external links are nofollow'd (unless the domain is in the white list).
-Version: 1.0
+Version: 1.0.1
 Author: David Herron
 Author URI: http://davidherron.com
 License: GPL2
