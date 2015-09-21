006fb4302efbfda2942310782f5409b4a2bbf901
David Herron <david@davidherron.com>
2015-09-20 17:05:21 -0700

    Update nofollow-external-link.php

--- nofollow-external-link.php
+++ nofollow-external-link.php
@@ -8,6 +8,20 @@ Author: David Herron
 Author URI: http://davidherron.com/wordpress
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 */
 
 function dh_nf_admin_sidebar() {
